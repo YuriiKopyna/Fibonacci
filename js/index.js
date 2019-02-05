1ea2f240ad303c384eb7dba341e5a710dba7ef34
Yurii <diesel6@i.ua>
2019-02-05 09:52:40 +0000

    fixed function fibRec()

--- js/index.js
+++ js/index.js
@@ -1,26 +1,27 @@
-const n = parseInt(prompt('Введіть порядковий номер числа Фібоначчі: '));
-first = 1;
-second = 1;
+// function fibNum(first, second, next){
+// const n = parseInt(prompt('Введіть порядковий номер числа Фібоначчі: '));
+// first = 1;
+// second = 1;
 
-for (let i = 3; i <= n; i++) {
-    next = first + second;
-    if (i < n) {
-        first = second;
-        second = next;
-    } else {
-        if (i === n) {
-            alert(next);
-        } else {
-            alert('Невідоме значення');
-        }
-    }
-}
-
+// for (let i = 3; i <= n; i++) {
+//     next = first + second;
+//     if (i < n) {
+//         first = second;
+//         second = next;
+//     } else {
+//         if (i === n) {
+//             alert(next);
+//         } else {
+//             alert('Невідоме значення');
+//         }
+//     }
+// }
+// }
 function fibRec(n) {
-    return fibRec(n-1)+fibRec(n-2);
-    if (n >= 1) {
-        alert.fibRec(n);
-    }else {
-        alert('')
+    console.log(n);
+    if (n <= 2) {
+        return 1;
     }
+    return fibRec(n-1)+fibRec(n-2);
 }
+console.log(fibRec(6));
